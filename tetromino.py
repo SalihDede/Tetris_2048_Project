e544ba8090183dba896f83c1d0321046644fa422
Wemijinia <yussufkartall03@gmail.com>
2024-04-08 21:22:11 +0300

    Added Tetrominos
    Added the 4 remaining tetromino shapes. In total, we have 7 tetromino shapes.

--- tetromino.py
+++ tetromino.py
@@ -15,28 +15,51 @@ class Tetromino:
       # determine the occupied (non-empty) cells in the tile matrix based on
       # the shape of this tetromino (see the documentation given with this code)
       occupied_cells = []
-
       if self.type == 'I':
          n = 4  # n = number of rows = number of columns in the tile matrix
-         # shape of the tetromino I in its initial rotation state
+         # shape of the tetromino I in its initial orientation
          occupied_cells.append((1, 0))  # (column_index, row_index)
          occupied_cells.append((1, 1))
          occupied_cells.append((1, 2))
          occupied_cells.append((1, 3))
       elif self.type == 'O':
          n = 2  # n = number of rows = number of columns in the tile matrix
-         # shape of the tetromino O in its initial rotation state
+         # shape of the tetromino O in its initial orientation
          occupied_cells.append((0, 0))  # (column_index, row_index)
          occupied_cells.append((1, 0))
          occupied_cells.append((0, 1))
          occupied_cells.append((1, 1))
       elif self.type == 'Z':
          n = 3  # n = number of rows = number of columns in the tile matrix
-         # shape of the tetromino Z in its initial rotation state
-         occupied_cells.append((0, 1))  # (column_index, row_index)
+         # shape of the tetromino Z in its initial orientation
+         occupied_cells.append((0, 0))  # (column_index, row_index)
+         occupied_cells.append((1, 0))
+         occupied_cells.append((1, 1))
+         occupied_cells.append((2, 1))
+      elif self.type == 'J':
+         n = 3
+         occupied_cells.append((1, 0))
          occupied_cells.append((1, 1))
          occupied_cells.append((1, 2))
-         occupied_cells.append((2, 2))
+         occupied_cells.append((0, 2))
+      elif self.type == 'L':
+         n = 3
+         occupied_cells.append((0, 0))
+         occupied_cells.append((0, 1))
+         occupied_cells.append((0, 2))
+         occupied_cells.append((1, 2))
+      elif self.type == 'S':
+         n = 3
+         occupied_cells.append((0, 1))
+         occupied_cells.append((1, 1))
+         occupied_cells.append((1, 0))
+         occupied_cells.append((2, 0))
+      elif self.type == 'T':
+         n = 3
+         occupied_cells.append((0, 0))
+         occupied_cells.append((1, 0))
+         occupied_cells.append((2, 0))
+         occupied_cells.append((1, 1))
       # create a matrix of numbered tiles based on the shape of this tetromino
       self.tile_matrix = np.full((n, n), None)
       # create the four tiles (minos) of this tetromino and place these tiles
